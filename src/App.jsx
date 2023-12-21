f7e820071f5b1f9ced6afc6ff281f3a08b1c5679
ahihi <bdsthocu20@gmail.com>
2023-12-21 13:57:38 +0700

    #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+import styled from "styled-components"
+
+// const Container = styled.div({
+//   height: "100vh",
+//   backgroundColor: "rebeccapurple"
+// })
+
+const Container = styled.div`
+    height          : 100vh;
+    color: white;
+    background: url("./img/bg.jpeg");
+    scroll-snap-type: y mandatory;
+    scroll-behavior: smooth;
+    overflow-y: auto;
+    scrollbar-width: none;
+    &::-webkit-scrollbar{
+        display: none;
+    }
+`
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
   )
 }
 
