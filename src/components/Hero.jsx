c42d21910fd2dbc81edcba24608cbad66e1c5e46
ahihi <bdsthocu20@gmail.com>
2023-12-21 17:34:52 +0700

    #5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const Section = styled.div`
   height: 100vh;
@@ -121,27 +122,42 @@ const Img = styled.img`
 
 const Hero = () => {
 
-    return (
-        <Section>
-            <Navbar />
-            <Container>
-                <Left>
-                    <Title>Think. Make. Solve.</Title>
-                    <WhatWeDo>
-                        <Line src="./img/line.png" />
-                        <Subtitle>What we Do</Subtitle>
-                    </WhatWeDo>
-                    <Desc>
-                        we enjoy creating delightful, human-centered digital experiences.
-                    </Desc>
-                    <Button>Learn More</Button>
-                </Left>
-                <Right>
-                    <Img src="./img/moon.png" />
-                </Right>
-            </Container>
-        </Section>
-    )
+  return (
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we enjoy creating delightful, human-centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.6}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
+    </Section>
+  )
 }
 
 export default Hero;
--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styled from "styled-components";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
 
 
 const Section = styled.div`
@@ -77,27 +80,34 @@ const Button = styled.button`
 `;
 
 const Who = () => {
-    return (
-        <Section>
-            <Container>
-                <Left>
-
-                </Left>
-                <Right>
-                    <Title>Think outside the square space</Title>
-                    <WhatWeDo>
-                        <Line src="./img/line.png" />
-                        <Subtitle>Who we Are</Subtitle>
-                    </WhatWeDo>
-                    <Desc>
-                        a creative group of designers and developers with a passion for the
-                        arts.
-                    </Desc>
-                    <Button>See our works</Button>
-                </Right>
-            </Container>
-        </Section>
-    );
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube />
+              <OrbitControls enableZoom={false} autoRotate />
+            </Suspense>
+          </Canvas>
+        </Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who we Are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            a creative group of designers and developers with a passion for the
+            arts.
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Who;
