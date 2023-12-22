0ff7dd6d0b2600a874daf859c62f1ffb5704085f
ahihi <bdsthocu20@gmail.com>
2023-12-22 10:53:13 +0700

    #7

--- src/components/ProductDesign.jsx
+++ src/components/ProductDesign.jsx
@@ -4,19 +4,44 @@ import React, { Suspense, useRef } from 'react'
 import styled from "styled-components";
 import { IllustrationLogo } from "./3D/Illustration";
 
+const Desc = styled.div`
+  width: 200px;
+  height: 70px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  bottom: 30px;
+  right: 600px;
+
+  @media only screen and (max-width: 768px) {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+`;
+
 const ProductDesign = () => {
     const ref = useRef()
     return (
-        <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
-            <Suspense fallback={null}>
-                <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
-                    false
-                    <IllustrationLogo />
-                    false
-                </Stage>
-            </Suspense>
-            <OrbitControls ref={ref} enableZoom={false} autoRotate />
-        </Canvas>
+        <>
+            <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+                <Suspense fallback={null}>
+                    <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
+                        false
+                        <IllustrationLogo />
+                        false
+                    </Stage>
+                </Suspense>
+                <OrbitControls ref={ref} enableZoom={false} autoRotate />
+            </Canvas>
+            <Desc>
+                We design products with a strong focus on both world class design and
+                ensuring your product is a market success.
+            </Desc>
+        </>
     )
 }
 
--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -4,20 +4,44 @@ import React, { Suspense, useRef } from 'react'
 import styled from "styled-components";
 import { ReactLogo } from "./3D/React";
 
+const Desc = styled.div`
+  width: 200px;
+  height: 70px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  top: 20px;
+  right: 50px;
+
+  @media only screen and (max-width: 768px) {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+`;
 
 const WebDesign = () => {
     const ref = useRef()
     return (
-        <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
-            <Suspense fallback={null}>
-                <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
-                    false
-                    <ReactLogo />
-                    false
-                </Stage>
-            </Suspense>
-            <OrbitControls ref={ref} enableZoom={false} autoRotate />
-        </Canvas>
+        <>
+            <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+                <Suspense fallback={null}>
+                    <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
+                        false
+                        <ReactLogo />
+                        false
+                    </Stage>
+                </Suspense>
+                <OrbitControls ref={ref} enableZoom={false} autoRotate />
+            </Canvas>
+            <Desc>
+                We design products with a strong focus on both world class design and
+                ensuring your product is a market success.
+            </Desc>
+        </>
     )
 }
 
